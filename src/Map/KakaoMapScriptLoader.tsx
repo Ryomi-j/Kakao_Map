bb807f360069b73611b75c67b46c1d4348ed65ad
Ryomi-j <ryomi.j78@gmail.com>
2023-04-12 11:16:10 +0900

    hide appKey by using environment variable(env)

--- src/Map/KakaoMapScriptLoader.tsx
+++ src/Map/KakaoMapScriptLoader.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useEffect, useState } from "react";
 
 const KAKAO_MAP_SCRIPT_ID = "kakao-map-script";
-const KAKAO_MAP_APP_KEY = "a8a09f4ca59edaf73d046aafce30f3f6";
+const KAKAO_MAP_APP_KEY = process.env.KAKAO_MAP_KEY
 
 interface KakaoMapScriptLoaderProps {
 	children: ReactNode;
@@ -9,18 +9,19 @@ interface KakaoMapScriptLoaderProps {
 
 const KakaoMapScriptLoader = (props: KakaoMapScriptLoaderProps) => {
 	const [mapScriptLoaded, setMapScriptLoaded] = useState(false);
-
+	
 	useEffect(() => {
 		// 이미 지도가 로드된 경우 처리
 		const mapScript = document.getElementById(KAKAO_MAP_SCRIPT_ID);
 		if (mapScript && !window.kakao) return;
-
+		
 		// <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=발급받은 APP KEY를 넣으시면 됩니다."></script>
 		const script = document.createElement("script");
 		script.id = KAKAO_MAP_SCRIPT_ID;
-
-		// libraries=sevices : api 호출을 위함
-		script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_MAP_APP_KEY}&libraries=services&autoload=false`;
+		
+		// libraries=services : api 호출을 위함
+		script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_MAP_APP_KEY}&libraries=services&autoload=false`
+		console.log(script)
 		script.onload = () => {
 			window.kakao.maps.load(() => {
 				setMapScriptLoaded(true);
